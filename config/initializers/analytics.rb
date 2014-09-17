ce8ae36e9fa31ddadf256283c018801db0cbcb7a
chavikram <chavikram@gmail.com>
2014-09-17 15:51:20 +0530

    v3

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,9 +1,8 @@
-#module Rails::Application::Config
-#  class Analytics < Settingslogic
-#    source "#{Rails.root}/config/analytics.yml"
-#    namespace Rails.env
-#    load!
-#  end
-#end
-
-#Rails.application.config.analytics = Rails::Application::Config::Analytics
+module Rails::Application::Config
+  class Analytics < Settingslogic
+    source "#{Rails.root}/config/analytics.yml"
+    namespace Rails.env
+    load!
+  end
+end
+Rails.application.config.analytics = Rails::Application::Config::Analytics
